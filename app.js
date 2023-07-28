95b97cf2ea933a8158ebfe7236db352b78aa408b
ShingsiHairolla <93879495+Shyngys001@users.noreply.github.com>
2023-07-28 16:09:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,105 +1,90 @@
-//Gloval Variables
-
-var input=document.getElementById("input");
-
-var color=document.getElementById("color");
-
-var show=document.getElementById("showMsg");
-
-
-var top=document.getElementById("top");
-let out = document.getElementById("out");
+// Global Variables
+var input = document.getElementById("input");
+var color = document.getElementById("color");
+var show = document.getElementById("showMsg");
+var top = document.getElementById("top"); // Avoid using the variable name 'top', as it's a reserved keyword
+var out = document.getElementById("out");
+var favcolor5 = document.getElementById("favcolor5");
+var favcolor6 = document.getElementById("favcolor6");
+var favcolor7 = document.getElementById("favcolor7");
+var favcolor8 = document.getElementById("favcolor8");
+var favcolor1 = document.getElementById("favcolor1");
+var favcolor2 = document.getElementById("favcolor2");
+var favcolor3 = document.getElementById("favcolor3");
+var favcolor4 = document.getElementById("favcolor4");
+var showMsg = document.getElementById("showMsg");
+var fonts1 = document.getElementById("fonts1");
+var fonts2 = document.getElementById("fonts2");
+var fonts3 = document.getElementById("fonts3");
+var fonts4 = document.getElementById("fonts4");
 
 //**********************
-//  OnType Function
-//(To capture user_input)
+// OnType Function
+// (To capture user_input)
 //**********************
 
-
-function showText()
-{
-  var input=document.getElementById("input");
-var showMsg=document.getElementById("showMsg").innerHTML=input.value;
-//****************
-//  Button Code
-//****************
-
-//Button 01
-  var fonts1=document.getElementById("fonts1").addEventListener("click",() =>
-{
-  var showMsg=document.getElementById("showMsg");
-  showMsg.style.fontFamily = "'Style Script', cursive";
-})
-
-//Button 02
-
-var fonts2=document.getElementById("fonts2").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
-showMsg.style.fontFamily = " 'Palette Mosaic', cursive";
-})
-
-//Button 03
-
-var fonts3=document.getElementById("fonts3").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
-showMsg.style.fontFamily = "'Comfortaa', cursive";
-})
-
-//Button 04
-
-var fonts4=document.getElementById("fonts4").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
-showMsg.style.fontFamily = "'Shadows Into Light', cursive";
-})
+function showText() {
+  var input = document.getElementById("input");
+  var showMsg = document.getElementById("showMsg").innerHTML = input.value;
+  //****************
+  // Button Code
+  //****************
 }
 
-//**********************
-//Button Get's End Here
-//**********************
+// Event Listeners for Font Buttons
+fonts1.addEventListener("click", () => {
+  showMsg.style.fontFamily = "'Style Script', cursive";
+});
 
+fonts2.addEventListener("click", () => {
+  showMsg.style.fontFamily = "'Palette Mosaic', cursive";
+});
 
-//**************************
-// :) ->  Color 01 Start :-)
+fonts3.addEventListener("click", () => {
+  showMsg.style.fontFamily = "'Comfortaa', cursive";
+});
 
-var favcolor1=document.getElementById("favcolor1").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
- show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff,0 0 2rem #E62E2D,0 0 0.8rem #E62E2D,0 0 2.8rem #E62E2D,inset 0 0 1.3rem #E62E2D";
- showMsg.style.color = "#E62E2D";
-})
+fonts4.addEventListener("click", () => {
+  showMsg.style.fontFamily = "'Shadows Into Light', cursive";
+});
 
-// :) ->  Color 02
+// Event Listeners for Color Buttons
+favcolor1.addEventListener("click", () => {
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem #E62E2D, 0 0 0.8rem #E62E2D, 0 0 2.8rem #E62E2D, inset 0 0 1.3rem #E62E2D";
+  showMsg.style.color = "#E62E2D";
+});
 
-var favcolor2=document.getElementById("favcolor2").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
-show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff,0 0 2rem #567DA7,0 0 0.8rem #567DA7,0 0 2.8rem #567DA7,inset 0 0 1.3rem #567DA7";
- showMsg.style.color = "#567DA7";
+favcolor2.addEventListener("click", () => {
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem blue, 0 0 0.8rem blue, 0 0 2.8rem blue, inset 0 0 1.3rem blue";
+  showMsg.style.color = "blue";
+});
 
-})
+favcolor3.addEventListener("click", () => {
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem yellow, 0 0 0.8rem yellow, 0 0 2.8rem yellow, inset 0 0 1.3rem yellow";
+  showMsg.style.color = "yellow";
+});
 
-// :) ->  Color 03
+favcolor4.addEventListener("click", () => {
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem #E72EE9, 0 0 0.8rem #E72EE9, 0 0 2.8rem #E72EE9, inset 0 0 1.3rem #E72EE9";
+  showMsg.style.color = "#E72EE9";
+});
 
-var favcolor3=document.getElementById("favcolor3").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
-show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff,0 0 2rem #E7DB57,0 0 0.8rem #E7DB57,0 0 2.8rem #E7DB57,inset 0 0 1.3rem #E7DB57";
- showMsg.style.color = "#E7DB57";
+favcolor5.addEventListener("click", () => {
+  showMsg.style.color = 'orange';
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem orange, 0 0 0.8rem orange, 0 0 2.8rem orange, inset 0 0 1.3rem orange";
+});
 
-})
+favcolor6.addEventListener("click", () => {
+  showMsg.style.color = 'white';
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem white, 0 0 0.8rem white, 0 0 2.8rem white, inset 0 0 1.3rem white";
+});
 
-// :) ->  Color 04
+favcolor7.addEventListener("click", () => {
+  showMsg.style.color = 'green';
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem green, 0 0 0.8rem green, 0 0 2.8rem green, inset 0 0 1.3rem green";
+});
 
-var favcolor4=document.getElementById("favcolor4").addEventListener("click",() =>
-{
-var showMsg=document.getElementById("showMsg");
-show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff,0 0 2rem #E72EE9,0 0 0.8rem #E72EE9,0 0 2.8rem #E72EE9,inset 0 0 1.3rem #E72EE9";
- showMsg.style.color = "#E72EE9";
-})
-
-//**********************
-//Color's Code End Here
-//**********************
+favcolor8.addEventListener("click", () => {
+  showMsg.style.color = 'pink';
+  show.style.boxShadow = "0 0 .2rem #fff, 0 0 .2rem #fff, 0 0 2rem pink, 0 0 0.8rem pink, 0 0 2.8rem pink, inset 0 0 1.3rem pink";
+});
